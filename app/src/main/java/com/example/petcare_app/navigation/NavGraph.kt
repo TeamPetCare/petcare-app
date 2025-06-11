7b28726af15ec53eecf706355f31f459aab8e154
Isaac <Isaac.silvajr@sptech.school>
2025-06-10 21:11:58 -0300

    Agendamento POST, Sem método de pagamento completo

--- app/src/main/java/com/example/petcare_app/navigation/NavGraph.kt
+++ app/src/main/java/com/example/petcare_app/navigation/NavGraph.kt
@@ -24,6 +24,10 @@
     import com.example.petcare_app.ui.screens.NotificationScren
     import com.example.petcare_app.ui.screens.PlansScreen
     import com.example.petcare_app.ui.screens.SchedulesScreen
+//    import com.example.petcare_app.ui.screens.CreateScheduleScreen
+//    import com.example.petcare_app.ui.screens.SchedulePaymentScreen
+//    import com.example.petcare_app.ui.screens.PaymentConfirmationScreen
+//    import com.example.petcare_app.ui.screens.ScheduleSuccessScreen
     import com.example.petcare_app.ui.screens.SettingsScreen
     import com.example.petcare_app.ui.screens.SignUpPetScreen
     import com.example.petcare_app.ui.screens.SignUpUserScreen
@@ -42,6 +46,10 @@
         object LoadingToAppHome : Screen("loadingtoapphome")
         object Plans : Screen("plans")
         object Schedules : Screen("schedules") // Tela de Agendamentos
+        object CreateSchedule : Screen("create_schedule")
+        object SchedulePayment : Screen("schedule_payment")
+        object PaymentConfirmation : Screen("payment_confirmation")
+        object ScheduleSuccess : Screen("schedule_success")
         object Settings : Screen("settings") // Tela de Configurações
         object Notifications : Screen("notifications")
         object EditUser : Screen("editUser")
@@ -63,6 +71,10 @@
             composable(Screen.LoadingToAppHome.route) { LoadingScreenToAppHome(navController, Screen.HomeApp.route) }
             composable(Screen.Plans.route) { PlansScreen(navController) }
             composable(Screen.Schedules.route) { SchedulesScreen(navController) }
+//            composable(Screen.CreateSchedule.route) { CreateScheduleScreen(navController) }
+//            composable(Screen.SchedulePayment.route) { SchedulePaymentScreen(navController) }
+//            composable(Screen.PaymentConfirmation.route) { PaymentConfirmationScreen(navController) }
+//            composable(Screen.ScheduleSuccess.route) { ScheduleSuccessScreen(navController) }
             composable(Screen.Settings.route) { SettingsScreen(navController) }
             composable(Screen.Notifications.route) { NotificationScren(navController) }
             composable(Screen.EditUser.route) { EditUserScreen(navController, signUpViewModel) }
