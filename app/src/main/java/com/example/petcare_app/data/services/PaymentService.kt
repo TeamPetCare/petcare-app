b151f7c121db2097424ee5ff88731274f7a979f7
daniescobarx <dani.0paula31@gmail.com>
2025-06-15 21:30:29 -0300

    feat: add implementação de pagamento pix via mercado pago

--- app/src/main/java/com/example/petcare_app/data/services/PaymentService.kt
+++ app/src/main/java/com/example/petcare_app/data/services/PaymentService.kt
@@ -4,12 +4,15 @@ import com.example.petcare_app.data.dto.PixPaymentDTO
 import com.example.petcare_app.data.dto.SchedulePUTDTO
 import com.example.petcare_app.data.model.Payment
 import com.example.petcare_app.data.model.PaymentModel
+import com.example.petcare_app.data.model.PixPaymentRequest
+import com.example.petcare_app.data.model.PixPaymentResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Url
 
 interface PaymentService {
     @GET("/api/payments/{id}")
@@ -24,4 +27,10 @@ interface PaymentService {
         @Path("id") id: Int,
         @Body pixPaymentData: PixPaymentDTO
     ) : Response<Payment>
+
+    @POST("/api/payments/pix/13")
+    suspend fun createPixPayment(
+        @Header("Authorization") token: String,
+        @Body pixPaymentRequest: PixPaymentRequest
+    ): Response<PixPaymentResponse>
 }
